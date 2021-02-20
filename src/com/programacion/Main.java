2e6a3a3e8cfd214619b11de96bf531c0f59fc4d5
Joel Jorge Nunes Vázquez <jnunesvazquez@danielcastelao.org>
2021-02-20 15:47:26 +0100

    Introduciendo la libreria como submodulo

--- src/com/programacion/Main.java
+++ src/com/programacion/Main.java
@@ -1,8 +1,12 @@
 package com.programacion;
 
+import com.libreria.ObjetoConsola;
+import com.libreria.ObjetoPantalla;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        ObjetoConsola.imprimirPorConsola();
+        ObjetoPantalla.imprimirPorPantalla();
     }
 }
