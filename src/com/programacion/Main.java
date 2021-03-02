d3d63e221b234489e067f118012bdc9e34d9b5cf
Joel Jorge Nunes Vázquez <jnunesvazquez@danielcastelao.org>
2021-03-02 11:14:08 +0100

    Actualizando la clase con dos nuevos objetos

--- src/com/programacion/Main.java
+++ src/com/programacion/Main.java
@@ -1,12 +1,31 @@
 package com.programacion;
 
-import com.libreria.ObjetoConsola;
-import com.libreria.ObjetoPantalla;
+import com.libreria.FactoriaDeSalidas;
+import com.libreria.ISalida;
 
 public class Main {
 
     public static void main(String[] args) {
-        ObjetoConsola.imprimirPorConsola();
-        ObjetoPantalla.imprimirPorPantalla();
+
+        ISalida salidaConsola;
+        ISalida salidaVentana;
+        ISalida salidaImpresora;
+        ISalida salidaNombreConsola;
+        ISalida salidaNombreVentana;
+
+        salidaConsola = FactoriaDeSalidas.getProducto(FactoriaDeSalidas.CONSOLA, "Hola desde la fabrica");
+        salidaConsola.visualiza();
+
+        salidaVentana = FactoriaDeSalidas.getProducto(FactoriaDeSalidas.VENTANA, "Adios desde la fabrica");
+        salidaVentana.visualiza();
+
+        salidaImpresora = FactoriaDeSalidas.getProducto(FactoriaDeSalidas.IMPRESORA, "Imprimido");
+        salidaImpresora.visualiza();
+
+        salidaNombreConsola = FactoriaDeSalidas.getProducto(FactoriaDeSalidas.NOMBRECONSOLA, "");
+        salidaNombreConsola.visualiza();
+
+        salidaNombreVentana = FactoriaDeSalidas.getProducto(FactoriaDeSalidas.NOMBREPANTALLA,"");
+        salidaNombreVentana.visualiza();
     }
 }
